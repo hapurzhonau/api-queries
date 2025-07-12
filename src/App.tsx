107bfce6fe83cad560b04f3a280d4ce499afa410
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-07-12 19:09:25 +0300

    feat: implement errorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css';
-function App() {
-  return (
-    <>
-      <div>React</div>
-    </>
-  );
+import { Component } from 'react';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
+import MainPage from './pages/MainPage';
+
+class App extends Component {
+  render() {
+    return (
+      <ErrorBoundary>
+        <MainPage />
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,4 @@
+import { Component } from 'react';
+
+class MainPage extends Component {}
+export default MainPage;
