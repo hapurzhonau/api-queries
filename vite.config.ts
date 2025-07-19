274d4df00e2c3bae1c6f85d76d253e20f5409eee
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-07-19 18:42:53 +0300

    chore: add verbose test description displaying

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   test: {
+    reporters: ['verbose'],
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/__test__/setupTests.ts',
