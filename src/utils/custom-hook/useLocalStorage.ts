6fa168298d307b8193a08ca32945768eeef145be
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-07-27 15:17:54 +0300

    refactor: fix adding quotes around query parameter

--- src/utils/custom-hook/useLocalStorage.ts
+++ src/utils/custom-hook/useLocalStorage.ts
@@ -1,18 +1,15 @@
 import { useState } from 'react';
 
-export function useLocalStorage<T>(
+export function useLocalStorage(
   key: string,
-  initialValue: T
-): [T, (newValue: T) => void] {
+  initialValue: string
+): [string, (newValue: string) => void] {
   const storedValue = localStorage.getItem(key);
-  const initial: T = storedValue ? JSON.parse(storedValue) : initialValue;
-
-  const [value, setValue] = useState<T>(initial);
-
-  const setStoredValue = (newValue: T) => {
+  const initial = storedValue !== null ? storedValue : initialValue;
+  const [value, setValue] = useState<string>(initial);
+  const setStoredValue = (newValue: string) => {
     setValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
+    localStorage.setItem(key, newValue);
   };
-
   return [value, setStoredValue];
 }
