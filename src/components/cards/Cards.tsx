1f074ca776dc0d7e549d943a80b7b4cf829a1dca
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-07-27 19:10:09 +0300

    feat: enable click-to-open details sidebar for cards

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,3 +1,4 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import type { Character } from '../../interfaces/apiInterface';
 
 interface Props {
@@ -5,17 +6,32 @@ interface Props {
 }
 
 export const Cards = ({ cards }: Props) => {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const handleClick = (id: string) => {
+    const prevParams = new URLSearchParams(searchParams);
+
+    navigate({
+      pathname: `details/${id}`,
+      search: prevParams.toString(),
+    });
+  };
   return (
     <ul className="lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-4 grid">
       {cards.length ? (
         cards.map((card) => (
           <li key={card.id} className="bg-gray-700 rounded-sm p-1">
-            <img
-              className="w-full aspect-square object-cover rounded-t-sm"
-              src={card.image}
-              alt={card.name}
-            />
-            <p>{card.name}</p>
+            <button
+              onClick={() => handleClick(card.id.toString())}
+              className="block w-full"
+            >
+              <img
+                className="w-full aspect-square object-cover rounded-t-sm"
+                src={card.image}
+                alt={card.name}
+              />
+              <p>{card.name}</p>
+            </button>
           </li>
         ))
       ) : (
